2d508458b6f6574fc69c6e56ef0d1fdb529ffaf6
TheGovernor2 <efebasmuhammet4155@gmail.com>
2026-01-09 23:22:44 +0300

    Add files via upload

---
+++ birthday-master/birthday-master/config.js
@@ -0,0 +1,33 @@
+
+var config = {
+    
+    texts: [
+        "Azra 💗",      
+        "Gülüşüyle insanın içini ısıtan biri olduğuna eminimm",  
+        "Doğum günün kutlu olsunnn 🎂",
+        "İyi ki doğmuşsun:)",
+        "İyi ki varsın",
+        "İyiki tanısmısızz",
+        "çok iyi bi arkadaşsın",
+        "kalbi temiz birisin",
+        "umarım yüzün hep güler",
+        "nice mutlu yıllaraa😊🥹",
+        "azra~~🫠",
+    ],
+   
+    imgs: {
+        "azra~~🫠": "./imgs/AZRA.jpeg",
+        
+    },
+    
+    desc: {
+        turn_on: "Bas Bakimm😉",
+        play: "Müzik biraz saçma olduuğ",
+        bannar_coming: "yukardan bisey gelioooo",
+        balloons_flying: "Balonlarımız olmazsa olmazz🤭",
+        cake_fadein: "gönül isterdi daha güzellerini yapayım🙃",
+        light_candle: "ama beceremedim ya😞",
+        wish_message: "Doğum Günün Kutlu olsuunnn azraa😊",
+        story: "Sana bi mesajıımmm vaarr",
+    }
+};
